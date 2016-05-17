41a3da557073ee46bdab2fdfc7a60b2c6d403ca3
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-05-18 00:59:00 +0200

    copy du patch sqlite au démarage

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 export env=prod
 
+cp sqlite-patch/* build/dep/
+
 chmod +775 ./build/libs/IoT-Challenge-Capgemini.jar
 
 java -cp ./build/dep/*:./build/libs/* -DPROD_MODE=true iot.challenge.application.ServeurApplication
