643c7934a907200d68494b16b8f8b2eb9c2d2cee
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-05-18 02:02:12 +0200

    unix port 80 protégé donc démarer avec sudo java

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ cp sqlite-patch/* build/dep/
 
 chmod +775 ./build/libs/IoT-Challenge-Capgemini.jar
 
-java -cp ./build/dep/*:./build/libs/* -DPROD_MODE=true iot.challenge.application.ServeurApplication
+sudo java -cp ./build/dep/*:./build/libs/* -DPROD_MODE=true iot.challenge.application.ServeurApplication
