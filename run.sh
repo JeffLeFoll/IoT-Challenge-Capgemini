509ec8d97ac7a01f561dc302e686a1af4267401e
Jean-François Le Foll <jeff.lefoll@outlook.fr>
2016-05-19 00:10:19 +0200

    variable environnement

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-export environment=prod
-
 cp sqlite-patch/* build/dep/
 
 chmod +775 ./build/libs/IoT-Challenge-Capgemini.jar
 
-sudo java -cp ./build/dep/*:./build/libs/* -DPROD_MODE=true iot.challenge.application.ServeurApplication
+sudo java -cp ./build/dep/*:./build/libs/* -DPROD_MODE=true -Denv=prod iot.challenge.application.ServeurApplication
